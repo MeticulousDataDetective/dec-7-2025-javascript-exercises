de10d3d5c52ff0c9bb209eef73dc2c6876fe40c5
MeticulousDataDetective <rikkilance.montero@gmail.com>
2026-01-12 15:01:03 +0800

    Add and confirm all my solutions to three exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(s,b) {
+    if(!Number.isInteger(s)||!Number.isInteger(b)||s<0||b<0) return "ERROR";
+    if(b<s){
+        const tem=s;
+        s=b;
+        b=tem;
+    }
+    let sum=0;
+    for(let i=s;i<=b;++i){
+        sum+=i;
+    }
+    return sum; 
 };
 
 // Do not edit below this line
